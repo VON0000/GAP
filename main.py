041c5d98b592ab6fe947cc4113d03190cce5e759
VON0000 <zhanglu.930@outlook.com>
2023-10-10 12:48:43 +0800

    style: reform with black

--- main.py
+++ main.py
@@ -10,22 +10,24 @@ seuil = 28
 regulation = 3
 quarter = 0
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Specify the folder path
-    folder_path = './data/error-in-data'
+    folder_path = "./data/error-in-data"
     # folder_path = './data/error-in-data/buffer'
 
     # Iterate through the files in the folder
     for filename in os.listdir(folder_path):
-        if filename.endswith('.csv'):  # Check if the file ends with .csv
+        if filename.endswith(".csv"):  # Check if the file ends with .csv
             filename = os.path.join(folder_path, filename)
 
             # Generate initial solution
-            genernate_solution, pattern = GenernateSolution.generante_solution(filename, regulation, seuil, quarter,
-                                                                               part, delta)
+            genernate_solution, pattern = GenernateSolution.generante_solution(
+                filename, regulation, seuil, quarter, part, delta
+            )
 
             # Reallocate through iteration
-            re_solution = reallocation.reallocation(filename, seuil, part, delta, genernate_solution, regulation,
-                                                    pattern)
+            re_solution = reallocation.reallocation(
+                filename, seuil, part, delta, genernate_solution, regulation, pattern
+            )
 
             # Add new flights
