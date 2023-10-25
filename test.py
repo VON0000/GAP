76b82730456584a5bdcf94ce65656b3702807dd2
VON0000 <zhanglu.930@outlook.com>
2023-10-25 21:49:20 +0800

    test: build test.py

---
+++ test.py
@@ -0,0 +1,44 @@
+from FlightIncrease.GetInterval import GetInterval
+
+HOUR = 60 * 60
+
+
+def test_get_interval():
+    instance = GetInterval(filename="./data/test.csv")
+    inst_list = instance.get_interval()
+    for i in inst_list:
+        assert i.begin_interval < i.end_interval
+        assert i.interval >= 900
+
+
+def test_get_interval_one():
+    instance = GetInterval(filename="./data/test.csv")
+    for r in instance.data["registration"]:
+        inst_list = instance._get_interval_one(r)
+        for i in inst_list:
+            assert i.interval >= 0
+
+
+def test_flight_list_sorted():
+    instance = GetInterval(filename="./data/test.csv")
+    flight_list = instance.flight_list_sorted("B9987")
+    for i in range(len(flight_list) - 1):
+        if instance.data["departure"][flight_list[i]] == "ZBTJ":
+            assert (
+                instance.data["ATOT"][flight_list[i]]
+                <= instance.data["ALDT"][flight_list[i + 1]]
+            )
+        else:
+            assert (
+                instance.data["ALDT"][flight_list[i]]
+                <= instance.data["ATOT"][flight_list[i + 1]]
+            )
+
+
+def test_get_data():
+    instance = GetInterval(filename="./data/test.csv")
+    for i in range(len(instance.data["callsign"])):
+        if instance.data["callsign"][i][-2:] == "ar":
+            assert instance.data["arrivee"][i] == "ZBTJ"
+        else:
+            assert instance.data["departure"][i] == "ZBTJ"
