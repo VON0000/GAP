e32c2973af6aedab28416a3f8a8a44eb7050ad04
VON0000 <zhanglu.930@outlook.com>
2023-10-27 16:00:54 +0800

    feat: determining whether two intervals overlap, improving process control

--- FlightIncrease/IncreaseFlight.py
+++ FlightIncrease/IncreaseFlight.py
@@ -7,33 +7,62 @@ from FlightIncrease.GetWingSpan import GetWingSpan
 from FlightIncrease.IntervalType import IntervalType
 
 
+def _is_overlapping(time1, time2) -> bool:
+    """
+    判断两个时间段是否有重叠
+    False 为没有冲突
+    True 为有冲突
+    """
+    if time1[0] <= time2[0] <= time1[1]:
+        return True
+    if time2[0] <= time1[0] <= time2[1]:
+        return True
+    return False
+
+
+def _conflict_half(
+    aug_inst: IntervalType, inst: IntervalType, gate: str, flag: bool
+) -> bool:
+    """
+    False 为没有冲突
+    True 为有冲突
+    """
+    if inst.gate == gate or inst.gate == re.findall(r"\d+", gate):
+        flag = _is_overlapping(
+            (inst.begin_interval, inst.end_interval),
+            (aug_inst.begin_interval, aug_inst.end_interval),
+        )
+    return flag
+
+
+def _conflict_all(
+    aug_inst: IntervalType, inst: IntervalType, gate: str, flag: bool
+) -> bool:
+    """
+    False 为没有冲突
+    True 为有冲突
+    """
+    if re.findall(r"\d+", inst.gate) == re.findall(r"\d+", gate):
+        flag = _is_overlapping(
+            (inst.begin_interval, inst.end_interval),
+            (aug_inst.begin_interval, aug_inst.end_interval),
+        )
+    return flag
+
+
 class IncreaseFlight:
     def __init__(self, filename: str):
         inst_interval = GetInterval(filename)
         inst_wingspan = GetWingSpan()
         self.interval = inst_interval.interval
         self.gatesize = inst_wingspan.gatesize
-        ...
+        self.increase_list = self.increase_flight()
 
-    def find_size(self, inst: IntervalType) -> list:
-        """
-        根据当前interval实例的wingspan，找到合适的停机坪
-        """
-        ...
-
-    def conflict_half(self, inst: IntervalType, gate: str, flag: bool) -> bool:
-        if inst.gate == gate or inst.gate == re.findall(r"\d+", gate):
-            ...
-        return flag
-
-    def conflict_all(self, inst: IntervalType, gate: str, flag: bool) -> bool:
-        if re.findall(r"\d+", inst.gate) == re.findall(r"\d+", gate):
-            ...
-        return flag
-
-    def find_conflict(self, gate: str) -> bool:
+    def find_conflict(self, aug_inst: IntervalType, gate: str) -> bool:
         """
         检查当前停机坪是否有与添加interval冲突的interval
+        False 为没有冲突
+        True 为有冲突
         """
         flag = False
         counter = 0
@@ -41,10 +70,10 @@ class IncreaseFlight:
             inst = self.interval[counter]
             # 414
             if not (re.search("L", gate) or re.search("R", gate)):
-                flag = self.conflict_all(inst, gate, flag)
+                flag = _conflict_all(aug_inst, inst, gate, flag)
             # 414L 414R
             else:
-                flag = self.conflict_half(inst, gate, flag)
+                flag = _conflict_half(aug_inst, inst, gate, flag)
             counter = counter + 1
         return flag
 
@@ -55,7 +84,7 @@ class IncreaseFlight:
         available_gate = []
         for i in range(len(self.gatesize["size_limit"])):
             if inst.wingspan <= self.gatesize["size_limit"][i]:
-                if not self.find_conflict(self.gatesize["gate"][i]):
+                if not self.find_conflict(inst, self.gatesize["gate"][i]):
                     available_gate.append(self.gatesize["gate"][i])
         if len(available_gate) == 0:
             return None
@@ -70,6 +99,7 @@ class IncreaseFlight:
         """
         increase_list = []
         for inst in self.interval:
-            if self.find_suitable_gate(inst):
-                increase_list.append(inst)
+            new_inst = self.find_suitable_gate(inst)
+            if new_inst is not None:
+                increase_list.append(new_inst)
         return increase_list
