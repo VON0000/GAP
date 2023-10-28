accd0d6e41a91b964f1d5187842d55f9aff8a112
VON0000 <zhanglu.930@outlook.com>
2023-10-28 22:21:15 +0800

    build: output data to csv

--- FlightIncrease/__init__.py
+++ FlightIncrease/__init__.py
@@ -1,5 +1,42 @@
-def output():
+import numpy as np
+import pandas as pd
+
+
+def build_data(data: dict) -> dict:
+    ...
+    return data
+
+
+def output(interval_list: list, filename: str):
     """
     将结果输出到csv文件中
     """
-    ...
+    data = {
+        "data": [],
+        "callsign": [],
+        "departure": [],
+        "arrivee": [],
+        "TTOT": [],
+        "TLDT": [],
+        "ATOT": [],
+        "ALDT": [],
+        "Type": [],
+        "Wingspan": [],
+        "Airline": [],
+        "QFU": [],
+        "Parking": [],
+        "registration": [],
+    }
+    reference = pd.read_csv(filename)
+    reference = reference.to_dict(orient="list")
+    for i in interval_list:
+        callsign = i.callsign[:-2].rstrip()
+        poss_list = set(np.where(np.array(reference["callsign"]) == callsign)[0]) & set(
+            np.where(np.array(reference["registration"]) == i.registration)[0]
+        )
+        assert poss_list != set()
+        for p in poss_list:
+            if callsign[-2:] == "de" and reference["departure"][p] == "ZBTJ":
+                data = build_data(data)
+            if callsign[-2:] == "ar" and reference["arrivee"][p] == "ZBTJ":
+                data = build_data(data)
