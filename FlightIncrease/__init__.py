d0e55ac28ee7478189cce077c34bb51755c5daf7
VON0000 <zhanglu.930@outlook.com>
2023-10-29 18:05:29 +0800

    fix: change the path of input folder

--- FlightIncrease/__init__.py
+++ FlightIncrease/__init__.py
@@ -5,12 +5,13 @@ from FlightIncrease.IncreaseFlight import IncreaseFlight
 from FlightIncrease.OutPut import OutPut
 
 if __name__ == "__main__":
-    folder_path = "../data/error-in-data"
+    folder_path = "../data/results/gate_5_taxi_15"
     # folder_path = './data/error-in-data/buffer'
 
     for filename in os.listdir(folder_path):
-        match = re.search(r"process", filename, re.M | re.I)
-        if match is None and filename.endswith(".csv"):
+        match_process = re.search(r"process", filename, re.M | re.I)
+        match_pn = re.search(r"PN", filename, re.M | re.I)
+        if match_pn is None and filename.endswith(".csv") and match_process is None:
             filename = os.path.join(folder_path, filename)
             increase_list = IncreaseFlight(filename).increase_list
             OutPut(increase_list, filename)
