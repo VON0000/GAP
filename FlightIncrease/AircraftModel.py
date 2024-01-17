114f7244490e5cf6890d6dddf549d26b32c183ad
VON0000 <zhanglu.930@outlook.com>
2024-01-17 23:17:53 +0800

    feat: get aircraft model instances

--- FlightIncrease/AircraftModel.py
+++ FlightIncrease/AircraftModel.py
@@ -16,13 +16,26 @@ def get_aircraft_model() -> dict:
     return aircraft_model
 
 
+def get_model_inst_list() -> list:
+    aircraft_model = get_aircraft_model()
+    model_inst_list = []
+    for i in range(len(aircraft_model["model"])):
+        model_inst_list.append(
+            AircraftModel(
+                aircraft_model["model"][i],
+                aircraft_model["engine_type"][i],
+                aircraft_model["aircraft_type"][i],
+            )
+        )
+    return model_inst_list
+
 
 class AircraftModel:
-    def __init__(self):
-        self.model = ...
-        self.engine_type = ...
-        self.aircraft_type = ...
+    def __init__(self, model: str, engine_type: str, aircraft_type: str):
+        self.model = model
+        self.engine_type = engine_type
+        self.aircraft_type = aircraft_type
 
 
 if __name__ == "__main__":
-    get_aircraft_model()
+    get_model_inst_list()
