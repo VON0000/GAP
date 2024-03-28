cd341d2a3a91633f22a5304b8f6daee6b4583435
VON0000 <zhanglu.930@outlook.com>
2024-03-28 21:23:57 +0800

    fix: avoid error in circle import

---
+++ refactorGateAllocation/run.py
@@ -0,0 +1,10 @@
+import os
+
+if __name__ == "__main__":
+
+    folder_path = "../results/Traffic_GAP_2Pistes"
+
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".csv"):
+            filename = os.path.join(folder_path, filename)
+            ...
