6c894276baff5fe5994b300b38d647e17bd2d1d7
VON0000 <zhanglu.930@outlook.com>
2024-04-02 21:59:33 +0800

    feat: change pattern automatically

--- run_GAP.py
+++ run_GAP.py
@@ -10,35 +10,36 @@ if __name__ == "__main__":
     folder_path = "./data/error-in-data"
     out_path = "./results/re_Traffic_GAP_mix\\"
     seuil = 28
-    pattern = "MANEX"
+    pattern_list = ["MANEX", "PN_MANEX"]
 
     result_list = []
 
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".csv"):
-            tracemalloc.start()
+    for pattern in pattern_list:
+        for filename in os.listdir(folder_path):
+            if filename.endswith(".csv"):
+                tracemalloc.start()
 
-            quarter = 0
-            filename = os.path.join(folder_path, filename)
+                quarter = 0
+                filename = os.path.join(folder_path, filename)
 
-            data = get_data("data/error-in-data/gaptraffic-2017-08-06-new.csv")
-            init_result = GateAllocation(data, seuil, pattern).optimization()
-            last_result = init_result
+                data = get_data("data/error-in-data/gaptraffic-2017-08-06-new.csv")
+                init_result = GateAllocation(data, seuil, pattern).optimization()
+                last_result = init_result
 
-            while True:
-                last_result = ReAllocation(data, seuil, pattern, quarter, init_result, last_result).optimization()
-                result_list.append(last_result)
+                while True:
+                    last_result = ReAllocation(data, seuil, pattern, quarter, init_result, last_result).optimization()
+                    result_list.append(last_result)
 
-                quarter += 1
-                if quarter == 100:
-                    break
+                    quarter += 1
+                    if quarter == 100:
+                        break
 
-                current, peak = tracemalloc.get_traced_memory()
-                print(f"当前时刻为：{quarter}")
-                print(f"当前内存使用：{current / 10 ** 6}MB")
-                print(f"峰值内存使用：{peak / 10 ** 6}MB")
+                    current, peak = tracemalloc.get_traced_memory()
+                    print(f"当前时刻为：{quarter}")
+                    print(f"当前内存使用：{current / 10 ** 6}MB")
+                    print(f"峰值内存使用：{peak / 10 ** 6}MB")
 
-            OutPut(data, filename, out_path).output_process(result_list)
-            OutPut(data, filename, out_path).output_final(last_result)
+                OutPut(data, filename, out_path).output_process(result_list)
+                OutPut(data, filename, out_path).output_final(last_result)
 
-            tracemalloc.stop()
+                tracemalloc.stop()
