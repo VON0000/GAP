0597cf2a9c3c94c8405254ffbca39bdd62d3ae56
VON0000 <zhanglu.930@outlook.com>
2024-04-11 10:51:20 +0800

    docs: results for "2Pistes" and "sans_min_taxi"
    docs: results for "2Pistes"

--- dataCleaning/check_gap.py
+++ dataCleaning/check_gap.py
@@ -43,7 +43,7 @@ def check_gap(file, gate_d=GATE_D):
             last = cur
 
 
-folder_path = "../results/re_Traffic_GAP_16R/"
+folder_path = "../results/re_Traffic_GAP_2Pistes/"
 
 for filename in os.listdir(folder_path):
     match = re.search(r"process", filename, re.M | re.I)
