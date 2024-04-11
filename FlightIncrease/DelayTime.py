5bfe2a41d08433e9352d78c960f705f736991b39
VON0000 <zhanglu.930@outlook.com>
2024-04-12 02:15:26 +0800

    feat: comments

--- FlightIncrease/DelayTime.py
+++ FlightIncrease/DelayTime.py
@@ -1,10 +1,21 @@
 from typing import List
 
+import loguru
+
 from BasicFunction.IntervalType import IntervalBase
 from FlightIncrease.AircraftModel import AircraftModel
 
 
+@loguru.logger.catch()
 def delay_time(add_list: list, exist_interval: list) -> list:
+    """
+    只考虑降落航班之间的尾流间隔
+    | 1st acft → | A. L | A. M | A. H |
+    |------------|------|------|------|
+    |   A. L    | 60   | 120  | 180  |
+    |   A. M    | 60   | 60   | 90   |
+    |   A. H    | 60   | 60   | 90   |
+    """
     useful_interval = [instance for instance in exist_interval if instance.begin_callsign[-2:].rstrip() == "ar"]
 
     if len(add_list) == 0:
@@ -40,8 +51,9 @@ def find_insertion_location(useful_interval: List[IntervalBase], inst: IntervalB
     before_conflict = False
     for bi in before_inst_list:
         wake_turbulence = get_wake_turbulence(bi, inst)
+        # 增加的航班降落时间加上尾流间隔 与前一个航班冲突
         if bi.begin_interval <= inst.begin_interval < bi.begin_interval + wake_turbulence:
-            # update the delay time of the instance
+            # 更新interval 推迟尾流间隔的时间
             inst.begin_interval = bi.begin_interval + wake_turbulence
             before_conflict = True
 
@@ -49,12 +61,15 @@ def find_insertion_location(useful_interval: List[IntervalBase], inst: IntervalB
     after_conflict = False
     for ai in after_inst_list:
         wake_turbulence = get_wake_turbulence(inst, ai)
+        # 增加的航班降落时间加上尾流间隔 与后一个航班冲突
         if inst.begin_interval <= ai.begin_interval < inst.begin_interval + wake_turbulence:
-            # update the delay time of the instance
+            # 更新interval 推迟尾流间隔的时间
             wake_turbulence = get_wake_turbulence(ai, inst)
             inst.begin_interval = ai.begin_interval + wake_turbulence
             after_conflict = True
 
+    # 使用递归的原因: 对于一个interval来讲，先按顺序推到最晚的没有冲突的时间，此时时间对于整个list来讲不一定在什么位置，
+    # 于是再次循环，检查与list前面部分是否有冲突。
     if after_conflict or before_conflict:
         return find_insertion_location(useful_interval, inst)
 
@@ -63,7 +78,14 @@ def find_insertion_location(useful_interval: List[IntervalBase], inst: IntervalB
     return inst
 
 
-def get_wake_turbulence(front_inst: IntervalBase, end_inst: IntervalBase):
+def get_wake_turbulence(front_inst: IntervalBase, end_inst: IntervalBase) -> int:
+    """
+    | 1st acft → | A. L | A. M | A. H |
+    |------------|------|------|------|
+    |   A. L    | 60   | 120  | 180  |
+    |   A. M    | 60   | 60   | 90   |
+    |   A. H    | 60   | 60   | 90   |
+    """
     if AircraftModel(front_inst.aircraft_model).aircraft_type == "L":
         return 60
     if AircraftModel(front_inst.aircraft_model).aircraft_type == "M":
